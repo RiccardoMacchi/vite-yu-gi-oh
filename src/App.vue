4a78911a2f9555d48a2254f4706048d3d895f7fb
RiccardoMacchi <rick.mac97@outlook.com>
2024-07-09 16:24:29 +0200

    creazione store e popolazione con richiesta axios

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <script>
+// Import di axios
+import axios from 'axios';
+
 import AppHeader from './components/AppHeader.vue';
 import AppWrapCard from './components/AppWrapCard.vue';
+
+// Importazione dello store
+import { store } from './store';
 
 export default {
   name: "App",
   components: {
     AppHeader,
     AppWrapCard,
+  },
+  data(){
+    return {
+      store,
+    }
+  },
+  methods: {
+    axiosRequest(){
+      axios.get(store.apiUrl)
+          .then(response =>{
+              console.log(response.data.data);
+              store.cardList = response.data.data
+          })
+          .catch(error => {
+              console.log(error)
+          })
+        }
+  },
+  created(){
+    this.axiosRequest();
   }
+
 }
 </script>
 
 <template>
   <AppHeader />
+  <main>
+    <AppWrapCard />
+  </main>
 </template>
 
 <style lang="scss">
--- src/components/AppWrapCard.vue
+++ src/components/AppWrapCard.vue
@@ -1,12 +1,24 @@
 <script>
+import { store } from "../store";
+import AppCard from "./AppCard.vue"
 export default {
     name: "AppWrapCard",
+    components:{
+        AppCard,
+    },
+    data(){
+        return {
+            store,
+        }
+    }
 }
 </script>
 
 <template>
     <div class="container">
-
+        <div v-for="card in store.cardList">
+            <AppCard />
+        </div>
     </div>
 </template>
 
